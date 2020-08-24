d3b48a09fc627d553366430414420257c8ef7497
Azran Hossain <57951696+b14ck0ps@users.noreply.github.com>
2020-08-25 00:28:37 +0600

    update version PP

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:Fsite/profile_page.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColorDark: Colors.black,
+        fontFamily: "GoogleSansRegular",
+      ),
+      home: ProfilePage(),
+    );
+  }
+}
---
+++ lib/profile_page.dart
@@ -0,0 +1,295 @@
+import 'dart:html' as html;
+
+import 'package:Fsite/responsive_widget.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.black,
+        ),
+        drawer: ResponsiveWidget.isSmallScreen(context)
+            ? Drawer(
+                child: ListView(
+                  padding: const EdgeInsets.all(20),
+                  // children: navButtons(),
+                ),
+              )
+            : null,
+        body: SingleChildScrollView(
+          child: AnimatedPadding(
+            duration: Duration(seconds: 1),
+            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
+            child: ResponsiveWidget(
+              largeScreen: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  // NavHeader(navButtons: navButtons()),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  ProfileInfo(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                  ),
+                  SocialInfo(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NavHeader extends StatelessWidget {
+  final List<Widget> navButtons;
+
+  const NavHeader({Key key, this.navButtons}) : super(key: key);
+
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: ResponsiveWidget.isSmallScreen(context)
+            ? MainAxisAlignment.center
+            : MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          PKDot(),
+//          Spacer(),
+          if (!ResponsiveWidget.isSmallScreen(context))
+            Row(
+                // children: navButtons,
+                )
+        ],
+      ),
+    );
+  }
+}
+
+class PKDot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Text(
+          "b14ck0ps",
+          textScaleFactor: 2,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        AnimatedContainer(
+          duration: Duration(seconds: 1),
+          height: 8,
+          width: 8,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.orange,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class NavButton extends StatelessWidget {
+  final text;
+  final onPressed;
+  final Color color;
+
+  const NavButton(
+      {Key key,
+      @required this.text,
+      @required this.onPressed,
+      this.color = Colors.orange})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      child: Text(text),
+      borderSide: BorderSide(
+        color: color,
+      ),
+      onPressed: onPressed,
+      highlightedBorderColor: color,
+    );
+  }
+}
+
+class ProfileInfo extends StatelessWidget {
+  profileImage(context) => Container(
+        height: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        width: ResponsiveWidget.isSmallScreen(context)
+            ? MediaQuery.of(context).size.height * 0.25
+            : MediaQuery.of(context).size.width * 0.25,
+        decoration: BoxDecoration(
+          backgroundBlendMode: BlendMode.luminosity,
+          color: Colors.transparent,
+          image: DecorationImage(
+            image: AssetImage("assets/logo.png"),
+            alignment: Alignment.center,
+            fit: BoxFit.scaleDown,
+          ),
+        ),
+      );
+
+  final profileData = Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        "Welcome! My name is",
+        textScaleFactor: 2,
+        style: TextStyle(color: Colors.orange),
+      ),
+      Text(
+        "Azran\nHossain",
+        textScaleFactor: 5,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      Text(
+        "I am new to programming and web development.\n"
+        "Trying to learn by myself...\n"
+        "well, actualy by watching youtube tutorials LOL",
+        softWrap: true,
+        textScaleFactor: 1.5,
+        style: TextStyle(color: Colors.white70),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          RaisedButton(
+            shape: StadiumBorder(),
+            child: Text("Resume"),
+            color: Colors.red,
+            onPressed: () {
+              html.window.open(
+                  "https://drive.google.com/file/d/1FOc47NoJRrxq6xk_3NFeuybLEPykp1Qk",
+                  "GDE");
+            },
+            padding: EdgeInsets.all(10),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          OutlineButton(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+            shape: StadiumBorder(),
+            child: Text("Say Hi!"),
+            color: Colors.red,
+            onPressed: () {
+              html.window.open("https://fb.me/azran.mrx", "Pk");
+            },
+            padding: EdgeInsets.all(10),
+          )
+        ],
+      )
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[profileImage(context), profileData],
+      ),
+      smallScreen: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          profileImage(context),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          profileData
+        ],
+      ),
+    );
+  }
+}
+
+class SocialInfo extends StatelessWidget {
+  List<Widget> socialMediaWidgets() {
+    return [
+      NavButton(
+        text: "Github",
+        onPressed: () {
+          html.window.open("https://github.com/b14ck0ps", "Git");
+        },
+        color: Colors.blue,
+      ),
+      NavButton(
+        text: "Twitter",
+        onPressed: () {
+          html.window.open("https://twitter.com/b14ck0ps", "Twitter");
+        },
+        color: Colors.blue,
+      ),
+      NavButton(
+        text: "Facebook",
+        onPressed: () {
+          html.window.open("https://www.facebook.com/b14ck0ps", "Fb");
+        },
+        color: Colors.blue,
+      ),
+    ];
+  }
+
+  Widget copyRightText() => Text(
+        "",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.grey,
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: socialMediaWidgets(),
+          ),
+          copyRightText(),
+        ],
+      ),
+      smallScreen: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          ...socialMediaWidgets(),
+          copyRightText(),
+        ],
+      ),
+    );
+  }
+}
